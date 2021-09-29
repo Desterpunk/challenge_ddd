620ed5bd6a11d32b9c3a1de5770d50c0a54e5e8e
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 18:09:04 -0500

    Creación de el identificador RoomId

--- src/main/java/co/com/sofka/app/domain/reserve/value/RoomId.java
+++ src/main/java/co/com/sofka/app/domain/reserve/value/RoomId.java
@@ -3,4 +3,15 @@ package co.com.sofka.app.domain.reserve.value;
 import co.com.sofka.domain.generic.Identity;
 
 public class RoomId extends Identity {
+    public RoomId(){
+
+    }
+
+    private RoomId(String id){
+        super(id);
+    }
+
+    public static RoomId of(String id){
+        return new RoomId(id);
+    }
 }
