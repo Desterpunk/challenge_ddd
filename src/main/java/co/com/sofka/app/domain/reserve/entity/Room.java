c41074222fcda65517d7b26aee0438df76815307
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 18:29:37 -0500

    Creación de la entidad Room

--- src/main/java/co/com/sofka/app/domain/reserve/entity/Room.java
+++ src/main/java/co/com/sofka/app/domain/reserve/entity/Room.java
@@ -1,7 +1,32 @@
 package co.com.sofka.app.domain.reserve.entity;
 
+import co.com.sofka.app.domain.generic.Status;
+import co.com.sofka.app.domain.generic.Type;
+import co.com.sofka.app.domain.reserve.value.BedsAmount;
 import co.com.sofka.app.domain.reserve.value.RoomId;
 import co.com.sofka.domain.generic.Entity;
 
 public class Room extends Entity<RoomId> {
+    protected Type type;
+    protected Status status;
+    protected BedsAmount bedsAmount;
+
+    public Room(RoomId entityId, Type type, Status status, BedsAmount bedsAmount) {
+        super(entityId);
+        this.type = type;
+        this.status = status;
+        this.bedsAmount = bedsAmount;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public BedsAmount getBedsAmount() {
+        return bedsAmount;
+    }
 }
