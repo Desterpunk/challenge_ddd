a35089b5b74457eec647291a70d8566eec33f0d3
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 17:02:44 -0500

    Creacion de la entidad Doctor

--- src/main/java/co/com/sofka/app/domain/register/entity/Doctor.java
+++ src/main/java/co/com/sofka/app/domain/register/entity/Doctor.java
@@ -1,7 +1,39 @@
 package co.com.sofka.app.domain.register.entity;
 
+import co.com.sofka.app.domain.generic.IdType;
+import co.com.sofka.app.domain.generic.Name;
+import co.com.sofka.app.domain.generic.PhoneNumber;
 import co.com.sofka.app.domain.register.value.DoctorId;
+import co.com.sofka.app.domain.register.value.Specialty;
 import co.com.sofka.domain.generic.Entity;
 
 public class Doctor extends Entity<DoctorId> {
+    protected IdType idType;
+    protected Name name;
+    protected PhoneNumber phoneNumber;
+    protected Specialty specialty;
+
+    public Doctor(DoctorId entityId, IdType idType, Name name, PhoneNumber phoneNumber, Specialty specialty) {
+        super(entityId);
+        this.idType = idType;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.specialty = specialty;
+    }
+
+    public IdType getIdType() {
+        return idType;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public PhoneNumber getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Specialty getSpecialty() {
+        return specialty;
+    }
 }
