12c3519f4cda6ca28ad70618a44f7086bd3afd54
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 16:31:40 -0500

    Creacion de objeto de valor BreathingFrequency

--- src/main/java/co/com/sofka/app/domain/register/value/BreathingFrequency.java
+++ src/main/java/co/com/sofka/app/domain/register/value/BreathingFrequency.java
@@ -1,4 +1,33 @@
 package co.com.sofka.app.domain.register.value;
 
-public class BreathingFrequency ValueObject<String>{
+import co.com.sofka.app.domain.generic.Name;
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class BreathingFrequency implements ValueObject<Integer> {
+    private final Integer value;
+
+    public BreathingFrequency(Integer value){
+        this.value = Objects.requireNonNull(value, "La frecuencia respiratoria es necesaria");
+        if (this.value < 1 || this.value > 99){
+            throw new IllegalArgumentException("La frecuencia respiratoria tiene entre 1 y 2 carácteres");
+        }
+    }
+    public Integer value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BreathingFrequency breathingFrequency = (BreathingFrequency) o;
+        return Objects.equals(value, breathingFrequency.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
