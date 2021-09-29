02a125b7f2bd6339d298ebd52501c625348f9b16
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 17:32:01 -0500

    Creación de el Identificador KitId

--- src/main/java/co/com/sofka/app/domain/reserve/value/KitId.java
+++ src/main/java/co/com/sofka/app/domain/reserve/value/KitId.java
@@ -3,4 +3,15 @@ package co.com.sofka.app.domain.reserve.value;
 import co.com.sofka.domain.generic.Identity;
 
 public class KitId extends Identity {
+    public KitId(){
+
+    }
+
+    private KitId(String id){
+        super(id);
+    }
+
+    public static KitId of(String id){
+        return new KitId(id);
+    }
 }
