ac6237240d62e4e2b5a3b69093a0117b4eb5caf6
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 18:08:24 -0500

    Creación de el identificador ReserveId

--- src/main/java/co/com/sofka/app/domain/reserve/value/ReserveId.java
+++ src/main/java/co/com/sofka/app/domain/reserve/value/ReserveId.java
@@ -3,4 +3,15 @@ package co.com.sofka.app.domain.reserve.value;
 import co.com.sofka.domain.generic.Identity;
 
 public class ReserveId extends Identity {
+    public ReserveId(){
+
+    }
+
+    private ReserveId(String id){
+        super(id);
+    }
+
+    public static ReserveId of(String id){
+        return new ReserveId(id);
+    }
 }
