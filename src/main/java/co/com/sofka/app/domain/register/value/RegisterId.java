aabcc37335c70135079b4e663f5bfe2f3cff71b2
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 16:51:12 -0500

    Creacion del identificador RegisterId

--- src/main/java/co/com/sofka/app/domain/register/value/RegisterId.java
+++ src/main/java/co/com/sofka/app/domain/register/value/RegisterId.java
@@ -3,4 +3,15 @@ package co.com.sofka.app.domain.register.value;
 import co.com.sofka.domain.generic.Identity;
 
 public class RegisterId extends Identity {
+    public RegisterId(){
+
+    }
+
+    private RegisterId(String id){
+        super(id);
+    }
+
+    public static RegisterId of(String id){
+        return new RegisterId(id);
+    }
 }
