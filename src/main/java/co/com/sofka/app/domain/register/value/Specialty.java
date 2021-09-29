ce12e896d9d892c26119eb8ecd73a0ca14e72ca9
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 16:53:23 -0500

    Creacion del objeto de valor Specialty

--- src/main/java/co/com/sofka/app/domain/register/value/Specialty.java
+++ src/main/java/co/com/sofka/app/domain/register/value/Specialty.java
@@ -1,4 +1,35 @@
 package co.com.sofka.app.domain.register.value;
 
-public class Specialty ValueObject<String>{
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Specialty implements ValueObject<String> {
+    private final String value;
+
+    public Specialty(String value){
+        this.value = Objects.requireNonNull(value, "La especialidad es necesaria");
+        if (this.value.isBlank()){
+            throw new IllegalArgumentException("La especialidad es necesaria");
+        }
+        if (this.value.length() < 3 || this.value.length() >= 20){
+            throw new IllegalArgumentException("La especialidad tiene entre 4 y 20 carácteres");
+        }
+    }
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Specialty specialty = (Specialty) o;
+        return Objects.equals(value, specialty.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
