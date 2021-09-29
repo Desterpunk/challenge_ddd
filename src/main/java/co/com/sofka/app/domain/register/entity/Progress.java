5d170ef06a1fba41b27f4dea709556c263725898
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 17:06:10 -0500

    Creacion de la entidad Progress

--- src/main/java/co/com/sofka/app/domain/register/entity/Progress.java
+++ src/main/java/co/com/sofka/app/domain/register/entity/Progress.java
@@ -1,7 +1,35 @@
 package co.com.sofka.app.domain.register.entity;
 
-import co.com.sofka.app.domain.register.value.ProgressId;
+import co.com.sofka.app.domain.register.value.*;
 import co.com.sofka.domain.generic.Entity;
 
 public class Progress extends Entity<ProgressId> {
+    protected Status status;
+    protected Temperature temperature;
+    protected BreathingFrequency breathingFrequency;
+    protected Observation observation;
+
+    public Progress(ProgressId entityId, Status status, Temperature temperature, BreathingFrequency breathingFrequency, Observation observation) {
+        super(entityId);
+        this.status = status;
+        this.temperature = temperature;
+        this.breathingFrequency = breathingFrequency;
+        this.observation = observation;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Temperature getTemperature() {
+        return temperature;
+    }
+
+    public BreathingFrequency getBreathingFrequency() {
+        return breathingFrequency;
+    }
+
+    public Observation getObservation() {
+        return observation;
+    }
 }
