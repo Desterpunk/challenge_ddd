716a7b2fa1d7602be7bc1a2b17b5349d75998b7c
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 16:14:32 -0500

    Creacion de objeto de valor PhoneNumber

--- src/main/java/co/com/sofka/app/domain/generic/PhoneNumber.java
+++ src/main/java/co/com/sofka/app/domain/generic/PhoneNumber.java
@@ -1,4 +1,35 @@
 package co.com.sofka.app.domain.generic;
 
-public class PhoneNumber ValueObject<String>{
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class PhoneNumber implements ValueObject<String> {
+    public final String value;
+
+    public PhoneNumber(String value){
+        this.value = Objects.requireNonNull(value, "El numero de telefono es necesario");
+        if (this.value.isBlank()){
+            throw new IllegalArgumentException("El numero de telefono es necesario");
+        }
+        if (this.value.length() < 5 || this.value.length() > 15){
+            throw new IllegalArgumentException("El numero de telefono tiene entre 6 y 9 caracteres Ej: +573213391742");
+        }
+    }
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PhoneNumber phoneNumber = (PhoneNumber) o;
+        return Objects.equals(value, phoneNumber.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
