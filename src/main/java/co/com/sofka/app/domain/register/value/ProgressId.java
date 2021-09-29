bd205cc125dba77e4efb5c0fb435f0611b4b2bb4
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 16:49:42 -0500

    Creacion del identificador ProgressId

--- src/main/java/co/com/sofka/app/domain/register/value/ProgressId.java
+++ src/main/java/co/com/sofka/app/domain/register/value/ProgressId.java
@@ -3,4 +3,15 @@ package co.com.sofka.app.domain.register.value;
 import co.com.sofka.domain.generic.Identity;
 
 public class ProgressId extends Identity {
+    public ProgressId(){
+
+    }
+
+    private ProgressId(String id){
+        super(id);
+    }
+
+    public static ProgressId of(String id){
+        return new ProgressId(id);
+    }
 }
