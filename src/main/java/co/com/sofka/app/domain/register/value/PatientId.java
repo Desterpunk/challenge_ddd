b6422b105646f885fe0d3055ace0a368eb5a91d2
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 16:49:02 -0500

    Creacion del identificador PatientId

--- src/main/java/co/com/sofka/app/domain/register/value/PatientId.java
+++ src/main/java/co/com/sofka/app/domain/register/value/PatientId.java
@@ -3,4 +3,15 @@ package co.com.sofka.app.domain.register.value;
 import co.com.sofka.domain.generic.Identity;
 
 public class PatientId extends Identity {
+    public PatientId(){
+
+    }
+
+    private PatientId(String id){
+        super(id);
+    }
+
+    public static PatientId of(String id){
+        return new PatientId(id);
+    }
 }
--- src/main/java/co/com/sofka/app/domain/register/value/Pressure.java
+++
@@ -1,4 +0,0 @@
-package co.com.sofka.app.domain.register.value;
-
-public class Pressure ValueObject<String>{
-}
