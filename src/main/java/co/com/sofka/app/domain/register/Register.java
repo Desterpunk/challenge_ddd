b4b05051c1355778d13953390afc77c490c4bcd4
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 17:21:41 -0500

    Creacion de el agregado Register

--- src/main/java/co/com/sofka/app/domain/register/Register.java
+++ src/main/java/co/com/sofka/app/domain/register/Register.java
@@ -1,6 +1,32 @@
 package co.com.sofka.app.domain.register;
+import co.com.sofka.app.domain.generic.Day;
+import co.com.sofka.app.domain.register.entity.Doctor;
+import co.com.sofka.app.domain.register.entity.Progress;
+import co.com.sofka.app.domain.register.value.PatientId;
 import co.com.sofka.app.domain.register.value.RegisterId;
 import co.com.sofka.domain.generic.AggregateEvent;
+import co.com.sofka.domain.generic.DomainEvent;
+
+import java.util.List;
+import java.util.Set;
 
 public class Register extends AggregateEvent<RegisterId>{
+    protected PatientId patientId;
+    protected Day day;
+    protected Set<Doctor> doctors;
+    protected Set<Progress> progresses;
+
+    public Register(RegisterId registerId, Day day){
+        super(registerId);
+    }
+
+    private Register(RegisterId registerId) {
+        super(registerId);
+    }
+
+    public static Register from(RegisterId registerId, List<DomainEvent> events){
+        var register = new Register(registerId);
+        events.forEach(register::applyEvent);
+        return register;
+    }
 }
