0a5a7300b28352ddc42933d99a2d027959cb0077
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 18:06:45 -0500

    Creación de el objeto de valor PaymentStatus

--- src/main/java/co/com/sofka/app/domain/reserve/value/PaymentStatus.java
+++ src/main/java/co/com/sofka/app/domain/reserve/value/PaymentStatus.java
@@ -1,4 +1,27 @@
 package co.com.sofka.app.domain.reserve.value;
 
-public class PaymentStatus ValueObject<String>{
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class PaymentStatus implements ValueObject<Boolean> {
+    public final Boolean value;
+    public PaymentStatus(Boolean value){
+        this.value = Objects.requireNonNull(value, "El estado de pago es necesario");
+    }
+    public Boolean value() {
+        return value;
+    }
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PaymentStatus paymentStatus = (PaymentStatus) o;
+        return Objects.equals(value, paymentStatus.value);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
 }
