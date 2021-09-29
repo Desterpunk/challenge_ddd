9f6900d3ffeabab356acb0dd43ace683d098f1dd
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 17:02:30 -0500

    Creacion del objeto de valor Temperature

--- src/main/java/co/com/sofka/app/domain/register/value/Temperature.java
+++ src/main/java/co/com/sofka/app/domain/register/value/Temperature.java
@@ -1,4 +1,32 @@
 package co.com.sofka.app.domain.register.value;
 
-public class Temperature ValueObject<String>{
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Temperature implements ValueObject<Double> {
+    private final Double value;
+
+    public Temperature(Double value){
+        this.value = Objects.requireNonNull(value, "La Temperature es necesaria");
+        if (this.value < 1 || this.value > 99){
+            throw new IllegalArgumentException("La Temperatura tiene entre 1 y 2 carácteres");
+        }
+    }
+    public Double value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Temperature temperature = (Temperature) o;
+        return Objects.equals(value, temperature.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
