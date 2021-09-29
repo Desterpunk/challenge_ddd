2e192d5e64887bfd7f9ef8c416571da8dcb63c1c
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 16:33:47 -0500

    Creacion del identificador DoctorId

--- src/main/java/co/com/sofka/app/domain/register/value/DoctorId.java
+++ src/main/java/co/com/sofka/app/domain/register/value/DoctorId.java
@@ -3,4 +3,15 @@ package co.com.sofka.app.domain.register.value;
 import co.com.sofka.domain.generic.Identity;
 
 public class DoctorId extends Identity {
+    public DoctorId(){
+
+    }
+
+    private DoctorId(String id){
+        super(id);
+    }
+
+    public static DoctorId of(String id){
+        return new DoctorId(id);
+    }
 }
