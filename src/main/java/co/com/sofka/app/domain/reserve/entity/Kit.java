84b0cdad9d753223e03c37d4600e77d6a14a570d
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 18:26:30 -0500

    Creación de la entidad Kit

--- src/main/java/co/com/sofka/app/domain/reserve/entity/Kit.java
+++ src/main/java/co/com/sofka/app/domain/reserve/entity/Kit.java
@@ -1,7 +1,32 @@
 package co.com.sofka.app.domain.reserve.entity;
 
+import co.com.sofka.app.domain.generic.Type;
 import co.com.sofka.app.domain.reserve.value.KitId;
+import co.com.sofka.app.domain.reserve.value.Medicine;
+import co.com.sofka.app.domain.reserve.value.Supplie;
 import co.com.sofka.domain.generic.Entity;
 
 public class Kit extends Entity<KitId> {
+    protected Type type;
+    protected Medicine medicine;
+    protected Supplie supplie;
+
+    public Kit(KitId entityId, Type type, Medicine medicine, Supplie supplie) {
+        super(entityId);
+        this.type = type;
+        this.medicine = medicine;
+        this.supplie = supplie;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Medicine getMedicine() {
+        return medicine;
+    }
+
+    public Supplie getSupplie() {
+        return supplie;
+    }
 }
