ef316a1568a3fd13644218fd024bd6145a8f4153
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 16:20:55 -0500

    Creacion de objeto de valor Type

--- src/main/java/co/com/sofka/app/domain/generic/Type.java
+++ src/main/java/co/com/sofka/app/domain/generic/Type.java
@@ -1,4 +1,35 @@
 package co.com.sofka.app.domain.generic;
 
-public class Type ValueObject<String>{
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Type implements ValueObject<String> {
+    private final String value;
+
+    public Type(String value){
+        this.value = Objects.requireNonNull(value, "El tipo es necesario");
+        if (this.value.isBlank()){
+            throw new IllegalArgumentException("El tipo es necesario");
+        }
+        if (this.value.length() < 2 || this.value.length() >= 50){
+            throw new IllegalArgumentException("El tipo tiene entre 3 y 50 caracteres");
+        }
+    }
+
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Type type = (Type) o;
+        return Objects.equals(value, type.value);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
