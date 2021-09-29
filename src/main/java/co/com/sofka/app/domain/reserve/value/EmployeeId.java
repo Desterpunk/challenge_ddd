d09700a8daec70fc850892f22ab507c532178e0d
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 17:30:41 -0500

    Creación de el Identificador EmployeeId

--- src/main/java/co/com/sofka/app/domain/reserve/value/EmployeeId.java
+++ src/main/java/co/com/sofka/app/domain/reserve/value/EmployeeId.java
@@ -1,6 +1,18 @@
 package co.com.sofka.app.domain.reserve.value;
 
+import co.com.sofka.app.domain.register.value.RegisterId;
 import co.com.sofka.domain.generic.Identity;
 
 public class EmployeeId extends Identity {
+    public EmployeeId(){
+
+    }
+
+    private EmployeeId(String id){
+        super(id);
+    }
+
+    public static EmployeeId of(String id){
+        return new EmployeeId(id);
+    }
 }
