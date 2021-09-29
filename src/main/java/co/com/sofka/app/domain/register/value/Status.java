38b20850118622f24cd70eb212a4b079e33ad349
Jhon Serna <jhonalfaber@gmail.com>
2021-09-29 16:55:43 -0500

    Creacion del objeto de valor Status

--- src/main/java/co/com/sofka/app/domain/register/value/Status.java
+++ src/main/java/co/com/sofka/app/domain/register/value/Status.java
@@ -1,4 +1,35 @@
 package co.com.sofka.app.domain.register.value;
 
-public class Status ValueObject<String>{
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Status implements ValueObject<String> {
+    private final String value;
+
+    public Status(String value){
+        this.value = Objects.requireNonNull(value, "El estado es necesario");
+        if (this.value.isBlank()){
+            throw new IllegalArgumentException("El estado es necesario");
+        }
+        if (this.value.length() < 3 || this.value.length() >= 20){
+            throw new IllegalArgumentException("La estado tiene entre 4 y 20 carácteres");
+        }
+    }
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Status status = (Status) o;
+        return Objects.equals(value, status.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
